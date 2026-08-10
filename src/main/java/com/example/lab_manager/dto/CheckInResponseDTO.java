c8e93091ffaf5111374acacbc94aae737f789379
Guilherme Caixeta <guilhermecaixeta19@gmail.com>
2026-08-10 10:40:53 -0300

    feat: Implementação dos DTOs

--- src/main/java/com/example/lab_manager/dto/CheckInResponseDTO.java
+++ src/main/java/com/example/lab_manager/dto/CheckInResponseDTO.java
@@ -1,4 +1,18 @@
 package com.example.lab_manager.dto;
 
-public record CheckInResponseDTO() {
+import com.example.lab_manager.model.CheckIn;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record CheckInResponseDTO(
+     UUID id, UUID reserveId, LocalDateTime horarioChegada
+)
+{
+    public static CheckInResponseDTO fromEntity(CheckIn checkIn) {
+        return new CheckInResponseDTO(
+                checkIn.getId(), checkIn.getReserve().getId(), checkIn.getHorarioChegada()
+        );
+    }
+
 }
